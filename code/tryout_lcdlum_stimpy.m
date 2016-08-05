9da95f0286762eee1128d3b00fb3451451a54226
STIMPY NBP <stimpy@uos.de>
2016-08-05 17:31:27 +0200

    reaction time, race time, and some statistics. also ule added better comments and structure

--- code/tryout_lcdlum_stimpy.m
+++ code/tryout_lcdlum_stimpy.m
@@ -1,4 +1,4 @@
-% EEG = pop_fileio('/net/store/nbp/EEG/blind_spot/data/monitorSpeedCheck/benq120Hz/120hz gabor.cnt');
+%EEG = pop_fileio('/net/store/nbp/EEG/blind_spot/data/monitorSpeedCheck/benq120Hz/120hz gabor.cnt');
 if ispc
     p.eeglab = genpath('c:\users\behinger\Dropbox\phD\opto\scripts\toolbox\eeglab_git\');
     p.anteepimport = 'anteepimport1.10';
@@ -18,15 +18,24 @@ addpath(p.anteepimport)
 addpath('lib')
 %dataset = '/home/experiment/lcdlum/EEG-trigger-check/Test run/rand/20160802_1450.cnt'
 
-  EEG = pop_loadset('/home/experiment/lcdlum/data/rand.set');
+EEG = pop_loadset('/home/experiment/lcdlum/eeg_data/rand.set');
+%EEG = pop_loadset('/home/experiment/lcdlum/eeg_data/benq120hz.set');
 %EEG = pop_loadeep('data/grayvalues.cnt','triggerfile','on');
 
 eeglab redraw
 
 % parting of EEG Data into epochs
-%EEG2 = pop_epoch( EEG, {'100'}, [-0.05         0.05], 'newname', 'epochs', 'epochinfo', 'yes');
-EEG2 = pop_epoch( EEG, {'100'}, [0         0.05], 'newname', 'epochs', 'epochinfo', 'yes');
+
+%EEG2 = pop_epoch( EEG, {'100'}, [-0.05    0.05], 'newname', 'epochs', 'epochinfo', 'yes');
+%EEG2 = pop_epoch( EEG, {'95'}, [-0.03   0.09], 'newname', 'epochs', 'epochinfo', 'yes');
+for e = 1:length(EEG.event)
+    EEG.event(e).type = deblank(EEG.event(e).type);
+end
+EEG2 = pop_epoch( EEG, {'100'}, [0    0.09], 'newname', 'epochs', 'epochinfo', 'yes');
 % plotting normalized data
+
+%% 
+%-----plotting normalized data-----
 figure
 
 hold all
@@ -39,7 +48,7 @@ y_norm = y_on_zero / abs(min(y_on_zero(:)));
 x = plot(EEG2.times,squeeze(x_norm),'r');
 y = plot(EEG2.times,squeeze(y_norm),'g');
     
-    
+%transparency   
 for i = 1:length(x)
     x(i).Color(4) = 0.1;
 end
@@ -61,9 +70,7 @@ mDat_y = mean(squeeze(y_norm),2);
 plot(EEG2.times,mDat_x,'b')
 plot(EEG2.times,mDat_y,'b')
 
-for i = 1:length(y)
-    y(i).Color(4) = 0.1;
-end
+
 
 % title + label
 title('white-black-white-switch')
@@ -72,76 +79,64 @@ ylabel('voltage')
 
 
 %%
-threshBlack =  -0.01;
-threshWhite =  -0.99;
-
-%<>
-% raise time for black
-x_black = x_norm(:,:,1)<threshBlack;
-for j = 1:max(size(x_norm),3)
-    for k = 1:length(x_black)
-        if x_black(k) == 1
-            x_black(k) = 0;
-        else
-            break
-        end
-    end
-end
-y_black = y_norm(:,:,1)<threshBlack;
-for j = 1:size(y_norm,3)
-    for k = 1:length(y_black)
-        if y_black(k) == 1
-            y_black(k) = 0;
-        else
-            break
-        end
-    end
-end
-
-% raise time for white
-
-x_white = x_norm(:,:,1)<threshWhite;
-for j = 1:max(size(x_norm),3)
-    for k = 1:length(x_white)
-        if x_white(k) == 1
-            x_white(k) = 0;
-        else
-            break
-        end
-    end
-end
-
-y_to_white = nan(1,size(y_norm,3));
-y_white = y_norm(:,:,:)<threshWhite;
-for ep = 1:size(y_norm,3)
-    for k = 1:size(y_norm,2)
-        if y_white(1,k,ep) == 1
-            y_white(1,k,ep) = 0;
-        else
-            break
-        end
-    end
-    
-    y_to_white(ep) = find(y_white(1,:,ep),1,'first');
-
-end
 
 
-x_from_black = find(x_black,1,'first');
+% if thr1  < thr2
+%    a < thr
+% elseif strcmp(type,'greater')
+%     a !< thr
+% end
+
+%-----raise_time, reaction_time, response_time-----
+thresWhite =  -0.99;
+thresBlack =  -0.01;
+
+[raise_time_list1, reaction_time_list1] = find_raisetime(thresWhite, thresBlack, x_norm, EEG2.times);
+[raise_time_list2, reaction_time_list2] = find_raisetime(thresWhite, thresBlack, y_norm, EEG2.times);
+
+response_time1 = raise_time_list1 + reaction_time_list1;
+response_time2 = raise_time_list2 + reaction_time_list2;
 
 
+raise_mean1 = mean(raise_time_list1)
+raise_mean2 = mean(raise_time_list2)
 
-y_from_black = find(y_black,1,'first');
-y_to_white = find(y_white,1,'first');
+reaction_mean1 = mean(reaction_time_list1)
+reaction_mean2 = mean(reaction_time_list2)
 
-raise_time_x = EEG2.times(x_to_white) - EEG2.times(x_from_black)
-raise_time_y = EEG2.times(y_to_white) - EEG2.times(y_from_black)
+response_time1_mean = mean(response_time1)
+response_time2_mean = mean(response_time2)
+
+%%
+%-----delay for response, reaction and raise time-----
+
+delay_response_list = response_time1 - response_time2;
+delay_reaction_list = reaction_time_list1 - reaction_time_list2;
+delay_raise_list = raise_time_list1 - raise_time_list2;
+
+delay_response_mean = mean(delay_response_list)
+delay_reaction_mean = mean(delay_reaction_list)
+delay_raise_mean = mean(delay_raise_list)
+
+%%transparancy 
+%-----quantiles of raise-time, reaction-time, response-time------
+
+%quantiles raise-time
+raise_time_quantile1 = quantile(raise_time_list1,[0.05 0.95])
+raise_time_quantile2 = quantile(raise_time_list2,[0.05 0.95])
+%quantiles reaction-time
+reaction_time_quantile1 = quantile(reaction_time_list1,[0.05 0.95])
+reaction_time_quantile2 = quantile(reaction_time_list2, [0.05 0.95])
+%quantiles response-time
+response_time_quantile1 = quantile(response_time1,[0.05 0.95])
+response_time_quantile2 = quantile(response_time2,[0.05 0.95])
 
 
+%%
 % [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 0,'setname','EPOCHS','gui','off'); 
 % eeglab redraw
 print(sprintf('figures/%s_white2black.png',dataset),'-dpng')
-%%mDat = mean(EEG2.data,3);
+%%mDat = mean(EEG2.data,3);transparancy 
 figure; pop_erpimage(EEG2,1, [1],[[]],'IMAGEN',1,1,{},[],'' ,'yerplabel','\muV','erp','on','limits',[NaN NaN NaN NaN NaN NaN NaN NaN],'cbar','on','topo', { [1] EEG.chanlocs EEG.chaninfo } );
 
 
