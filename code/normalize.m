79eb76f61f46a9c2787b0b5519560e8ce432f163
udiallo <ule.diallo@gmail.com>
2016-08-16 18:07:44 +0200

    Delete normalize.m

--- code/normalize.m
+++
@@ -1,8 +0,0 @@
-function [x_norm, y_norm] = normalize(EEG2_data)
-    
-
-x_on_zero = EEG2_data(1,:,:) - (max(EEG2_data(1,:)));
-y_on_zero = EEG2_data(3,:,:) - (max(EEG2_data(3,:)));
-
-x_norm = x_on_zero / abs(min(x_on_zero(:)));
-y_norm = y_on_zero / abs(min(y_on_zero(:)));
