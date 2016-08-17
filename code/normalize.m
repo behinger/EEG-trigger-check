d7ab451044adcae6e801d9e15726bc64c31a55ec
ddiallo <ddiallo@callisto.neurobiopsychologie.uni-osnabrueck.de>
2016-08-17 14:20:54 +0200

    channelproblem solved

--- code/normalize.m
+++ code/normalize.m
@@ -2,7 +2,7 @@ function [x_norm, y_norm] = normalize(EEG2_data)
 
         
         x_norm = -17.4884 + -0.0091 * EEG2_data(1,:,:);
-        y_norm = -19.544  + -0.011033 * EEG2_data(3,:,:);
+        y_norm = -19.544  + -0.011033 * EEG2_data(2,:,:);
 end
 
 
